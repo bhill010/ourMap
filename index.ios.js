bb83531baa4f18f0f51451ab2cf11e78fcac72b9
Brandon Hill <bhill@brandons-air.attlocal.net>
2017-04-02 23:46:42 -0700

    Create app.js file that allows code to update in both android and ios simulataneously.

--- index.ios.js
+++ index.ios.js
@@ -1,17 +1,4 @@
-import React from 'react';
-import { AppRegistry, View, Text } from 'react-native';
+import { AppRegistry } from 'react-native';
+import App from './lib/app';
 
-const App = () => (
-  <View style={styles.containerStyle}>
-    <Text>ourMap</Text>
-  </View>
-);
-
-const styles = {
-  containerStyle: {
-    marginTop: 20
-  }
-};
-
-// Renders component to the device. Only the root component needs to be registered
 AppRegistry.registerComponent('ourMap', () => App);
