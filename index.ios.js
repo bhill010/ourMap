28282580e18a683bfaa8fae87da68dd934256645
John <johnitolee@gmail.com>
2017-04-03 12:52:39 -0700

    adds map

--- index.ios.js
+++ index.ios.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import { AppRegistry, View, Text } from 'react-native';
+import Map from './lib/components/map';
 
-const App = () => (
+import MapView from 'react-native-maps';
+
+const App = () => {
+  return (
   <View style={styles.containerStyle}>
-    <Text>ourMap</Text>
+    <Map />
   </View>
-);
+);};
 
 const styles = {
   containerStyle: {
-    marginTop: 20
-  }
+    marginTop: 20,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red'
+  },
+
 };
 
 // Renders component to the device. Only the root component needs to be registered
 AppRegistry.registerComponent('ourMap', () => App);
+
+
+// <MapView
+//   style={styles.map}
+//   initialRegion={{
+//     latitude: 37.78825,
+//     longitude: -122.4324,
+//     latitudeDelta: 0.0922,
+//     longitudeDelta: 0.0421,
+//   }}
+// >
+//   <MapView.Marker
+//     coordinate={{
+//       latitude: 37.78825,
+//       longitude: -122.4324
+//     }}>
+//
+//     <View style={styles.radius}>
+//       <View style={styles.marker} />
+//     </View>
+//
+//   </MapView.Marker>
+//
+// </MapView>
