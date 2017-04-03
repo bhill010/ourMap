691849d6c97a3fb54a65f1ef572f4da02bd8548c
John <johnitolee@gmail.com>
2017-04-03 14:39:41 -0700

    edit index page

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,5 @@
 // Renders component to the device. Only the root component needs to be registered
-=======
 import { AppRegistry } from 'react-native';
 import App from './lib/app';
 
->>>>>>> 2a16030172a2d49426d57e70607371ed849cad41
 AppRegistry.registerComponent('ourMap', () => App);
