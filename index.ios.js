4c9a7cc2174a525815ffda757b4ab1e8d446db31
John <johnitolee@gmail.com>
2017-04-03 14:12:28 -0700

    add map

--- index.ios.js
+++ index.ios.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { AppRegistry, View, Text } from 'react-native';
-import Map from './lib/components/map';
-
-import MapView from 'react-native-maps';
-
-const App = () => {
-  return (
-    <Map />
-);};
-
-
 // Renders component to the device. Only the root component needs to be registered
 =======
 import { AppRegistry } from 'react-native';
